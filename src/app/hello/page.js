8faef723aa705e0e9765216fb3a4794d4e7a30b7
uzair330 <uzair330@gmail.com>
2024-06-20 19:55:01 +0500

    initial coomit

---
+++ src/app/hello/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./page.module.css"
+const page = () => {
+  return (
+    <div className={styles.code}>
+      <h1>This is hello page</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <h1>
+
+    <main className={styles.main}>
+      
+hello world
+    </main>
+    </h1>
+  );
+}
